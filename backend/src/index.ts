8af9bbd30eebcb6e3df63a5e1f0452581f0d8995
Rudra Prasad Rout (Nokia) <rudra.prasad_rout@nokia.com>
2024-05-21 19:43:01 +0530

    frontend and db setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,9 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string ).then(()=>{console.log("connected to the db")})
 
 const app = express();
 app.use(express.json());
